ab2fe9e3a70c4ccabd6a1ce6be1906f9dc03fd8b
Colby Prather <colbynash01@gmail.com>
2019-12-02 15:05:20 -0700

    final detail page done

--- client/src/Details.js
+++ client/src/Details.js
@@ -8,20 +8,37 @@ class Details extends React.Component {
   constructor() {
     super();
     this.state = {
-        
+      storage: {contactInfo: {name: ''}}
     }
+  }
+
+  componentDidMount() {
+    let {_id} = this.props.match.params 
+    axios.get(`/classifiedItems/${_id}`).then(res => {
+      this.setState({
+        storage: res.data
+      })
+      console.log(res.data)
+    })
+
   }
 
 
 
 
 
-
-
   render() {
+    let {name, url, price, description} = this.state.storage
+    console.log(this.state.storage.contactInfo)
     return (
      <div>
-       details
+      <h2>{name}</h2>
+      <img src={url} alt='the pic'></img>
+      <h4>{price}</h4>
+      <p>{description}</p>
+      <h6>{this.state.storage.contactInfo.name}</h6>
+      <h6>{this.state.storage.contactInfo.phoneNum}</h6>
+      <h6>{this.state.storage.contactInfo.email}</h6>
      </div>
     )
   }
