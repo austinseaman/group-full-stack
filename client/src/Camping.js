7b621e4f08cc4d1a210616c21ddda5b131dfdab9
john connolly <jtconnolly82@gmail.com>
2019-12-03 16:55:16 -0700

    cleanup new changes

--- client/src/Camping.js
+++ client/src/Camping.js
@@ -10,11 +10,7 @@ class Camping extends Component {
     render() {
         const mappedCamping = this.props.camping.map((camp, i) => <Item key={i + camp.price} price={camp.price} name={camp.name} url={camp.url} _id={camp._id} />)
         return (
-<<<<<<< HEAD
-            <div className='items-container'>
-=======
             <div className="items-container">
->>>>>>> 310f2fbf6c1ae9220cc6a95f4a7caa4ac8d0caeb
                 {mappedCamping}
             </div>
         );
--- client/src/Cycling.js
+++ client/src/Cycling.js
@@ -10,11 +10,7 @@ class Cycling extends Component {
     render() {
         const mappedCycling = this.props.cycling.map((cycle, i) => <Item key={i + cycle.price} price={cycle.price} name={cycle.name} url={cycle.url} _id={cycle._id} />)
         return (
-<<<<<<< HEAD
             <div className='items-container'>
-=======
-            <div className="items-container">
->>>>>>> 310f2fbf6c1ae9220cc6a95f4a7caa4ac8d0caeb
                 {mappedCycling}
             </div>
         );
