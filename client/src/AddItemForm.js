764829e43293c14d4c847ea55ea8100ddb519fdb
Austin Seaman <apseaman0@gmail.com>
2019-12-04 09:34:18 -0700

    update tel input on form

--- client/src/AddItemForm.js
+++ client/src/AddItemForm.js
@@ -10,7 +10,7 @@ const AddItemForm = (props) => {
                     <input onChange={handleChange} type="text" name="url" value={url} placeholder="Image URL" required />
                     <input onChange={handleChange} type="text" name="price" value={price} placeholder="Price" required />
                     <input onChange={handleContactChange} type="text" name="name" value={props.contactInfo.name} placeholder="Your Name" required />
-                    <input onChange={handleContactChange} type="tel" pattern='[0-9]{3} [0-9]{3} [0-9]{4}' name="phoneNum" value={props.contactInfo.phoneNum} placeholder="Tel. eg. xxx-xxx-xxxx" required />
+                    <input onChange={handleContactChange} type="tel" pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' name="phoneNum" value={props.contactInfo.phoneNum} placeholder="Tel. eg. xxx-xxx-xxxx" required />
                     <input onChange={handleContactChange} type="email" name="email" value={props.contactInfo.email} placeholder="Email" required />
                     <br/>
                     <select name="category" onChange={handleCategoryChange}>
